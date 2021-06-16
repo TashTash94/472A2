37c6b21fdb19b2ee316505790e9abb14516ef99c
Tayyab <tayyab.h.qureshi@gmail.com>
2021-06-15 21:22:17 -0400

    Parsed through 13 Reasons Why  to retrieve review links, episode  name, and year

--- beautiful_soup.py
+++ beautiful_soup.py
@@ -1,39 +1,23 @@
 from requests import get
-url = 'https://www.imdb.com/title/tt6468322/episodes?season=1&ref_=tt_eps_sn_1'
-response = get(url)
-#print(response.text[:500])
-
 from bs4 import BeautifulSoup
-html_soup = BeautifulSoup(response.text, 'html.parser')
-type(html_soup)
+url = 'https://www.imdb.com/title/tt1837492/episodes?season={}&ref_=tt_eps_sn_{}'
 
-
-movie_containers = html_soup.find_all('div', class_ = 'article listo list')
-#episodes_container = html_soup.find_all('div', class_ = 'info')
-episodesContainerOdd = html_soup.find_all('div', class_ = 'list_item odd')
-episodesContainerEven = html_soup.find_all('div', class_ = 'list_item even')
-#print(type(movie_containers))
-#print(len(movie_containers))
-
-firstEpisode = episodesContainerOdd[0]
-secondEpisode = episodesContainerEven[0]
-
-#series_name = movie_containers.h3.a.text
-
-
-first_episode = firstEpisode.strong.text
-second_episode = secondEpisode.strong.text
-
-for i in episodesContainerEven:
-    print(episodesContainerOdd[len(episodesContainerOdd)-1].strong.text)
-    print(episodesContainerEven[len(episodesContainerEven)-1].strong.text)
-    len(episodesContainerEven)-1
-    len(episodesContainerEven)-1
-
-
-#for all in episodesContainerOdd:
-    #print(episodesContainerOdd.strong.text)
-#print(series_name)
-#print('Episode 1: ' + first_episode)
-#print('Episode 2: ' + second_episode)
-#print(len(episodesContainerOdd))
+for x in range(1,5,1):
+    response = get(url.format(x, x))
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    episodesContainerOdd = html_soup.find_all('div', class_ = 'list_item odd')
+    episodesContainerEven = html_soup.find_all('div', class_ = 'list_item even')
+    firstEpisode = episodesContainerOdd[0]
+    review_link = "https://www.imdb.com{}reviews?ref_=tt_urv"
+    print(f"Season {x}")
+    secondEpisode = episodesContainerEven[0]
+    for i in range(len(episodesContainerOdd)):
+        print(episodesContainerOdd[i].strong.text)
+        print(episodesContainerOdd[i].find('div', class_ = 'airdate').text.split(" ")[14].split("\n")[0])
+        print(review_link.format(episodesContainerOdd[i].strong.a['href']))
+        print('\n')
+        if i < len(episodesContainerEven):
+            print(episodesContainerEven[i].strong.text)
+            print(episodesContainerOdd[i].find('div', class_ = 'airdate').text.split(" ")[14].split("\n")[0])
+            print(review_link.format(episodesContainerEven[i].strong.a['href']))
+            print('\n')
